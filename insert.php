36c39c2bf5e4938161531d3c3b1d338e71ac5ad5
TonyTse-dev <zxie960@gmail.com>
2026-05-22 13:22:42 +0800

    Added insert functionality to the project.

---
+++ insert.php
@@ -0,0 +1,22 @@
+<?php
+    $host     = 'localhost';
+    $db_name  = 'smart-task-categorizer-db';
+    $username = 'root';
+    $password = '';
+
+    $link = mysqli_connect($host, $username, $password, $db_name);
+    
+    // Check if the form actually posted data
+        $task = $_POST['task'];
+        $date = $_POST['date'];
+
+    
+        $sql = "INSERT INTO tasks (Name, Due_date) VALUES ('$task', '$date')";
+        
+        
+        $insert = mysqli_query($link, $sql);
+
+        header("Location: home.html");
+        exit();
+    
+?>
